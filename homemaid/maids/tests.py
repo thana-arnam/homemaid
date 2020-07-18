acd8bc9c4809ed62a8ceff83836f61e4082ee60e
thanamat <thanamat@odds.team>
2020-07-18 14:26:08 +0700

    Add field profile_image to model maid

--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,6 +1,12 @@
+import os
 from datetime import date
+from unittest.mock import MagicMock
+
+from django.core.files import File
 from django.test import TestCase
+
 from .models import Maid
+
 
 class TestMaid(TestCase):
     def test_model_should_have_defined_fields(self):
@@ -22,3 +28,25 @@ class TestMaid(TestCase):
         self.assertEqual(maid.description, 'Super Maid of the year')        
         self.assertEqual(maid.certificate, 'Best Maid 2012')
         self.assertEqual(maid.salary, 3000)
+
+    def test_model_should_have_image_fields(self):
+        # Given
+        mock = MagicMock(spec=File)
+        mock.name = 'profile.png'
+
+        maid = Maid.objects.create(
+            name='BB',
+            profile_image=mock,
+            birthdate=date(1998, 4, 29),
+            description='Super Maid of the year',
+            certificate='Best Maid 2012',
+            salary=3000
+        )
+
+        # When
+        maid = Maid.objects.last()
+
+        # Then
+        self.assertEqual(maid.profile_image.name, 'profile.png')
+        
+        os.remove('profile.png')
