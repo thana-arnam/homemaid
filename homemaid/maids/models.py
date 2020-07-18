e22b92af50bdf58c5c999379d25b490944cceafd
thanamat <thanamat@odds.team>
2020-07-18 14:09:45 +0700

    Add models maid django

---
+++ homemaid/maids/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Maid(models.Model):
+    name = models.CharField(max_length=300)
+    birthdate = models.DateField()
+    description=models.TextField()
+    certificate=models.TextField()
+    salary=models.IntegerField()
