b5b25c20534968693890c3faff05204b09a2517c
thanamat <thanamat@odds.team>
2020-07-18 16:54:37 +0700

    Add view maid-list

--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -1,8 +1,12 @@
 # from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Maid
 
 class MaidListView(View):
     def get(self, request):
-        return HttpResponse()
+        html = ''
+        for m in Maid.objects.all():
+            html += f'<li>{m.name}</li>'
+        return HttpResponse(html)
     
