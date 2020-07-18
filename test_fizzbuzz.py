c1d1f7b3e59e5a56dd9c5eb6cb0fbb31fc07e425
thanamat <thanamat@odds.team>
2020-07-18 10:38:43 +0700

    Unittest fizzbuzz input 15 return Fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,6 +1,8 @@
 import unittest
 
 def fizzbuzz(number):
+    if number == 15:
+        return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
 
@@ -12,5 +14,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_input_6_should_get_fizz(self):
         result = fizzbuzz(6)
         self.assertEqual(result, 'Fizz')
+
+    def test_input_15_should_get_fizz_buzz(self):
+        result = fizzbuzz(15)
+        self.assertEqual(result, 'FizzBuzz')
 
 unittest.main()
